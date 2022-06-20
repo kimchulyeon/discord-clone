bc3545ae7e44fc3562c4835154f73b3a6e739b59
kim <kim@kimui-MacBookPro.local>
2022-06-20 17:53:37 +0900

    feat: POST regist logic

--- discord-clone-backend/controllers/auth/postRegister.js
+++ discord-clone-backend/controllers/auth/postRegister.js
@@ -4,10 +4,11 @@ const bcrypt = require('bcryptjs');
 // [6] 회원가입 로직
 const postRegister = async (request, response) => {
   try {
-    const { username, mail, password } = req.body;
+    const { username, mail, password } = request.body; // 유저에게 입력 받은 데이터
     // [6] EMAIL 중복 체크
     const userExists = await User.exists({ mail: mail.toLowerCase() });
 
+    // 중복되는 이메일일 경우 에러 처리
     if (userExists) {
       return response.status(409).send('E-mail already in use');
     }
@@ -15,7 +16,7 @@ const postRegister = async (request, response) => {
     // [6] 해쉬 비밀번호
     const encryptedPassword = await bcrypt.hash(password, 10);
 
-    // [6] 데이터베이스에 유저 정보 저장
+    // [6] 데이터베이스에 유저 정보 저장 (몽구스 스키마를 적용해서 생성)
     const user = await User.create({
       username,
       mail: mail.toLowerCase(),
@@ -34,7 +35,7 @@ const postRegister = async (request, response) => {
       },
     });
   } catch (err) {
-    return response.status(500).send('Error occured. Please try again');
+    return response.status(500).send(`Error occured. Please try again`);
   }
 };
 
