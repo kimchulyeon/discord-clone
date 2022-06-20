1694c70f72a730baaa22bafcc543045ec022b381
kim <kim@kimui-MacBookPro.local>
2022-06-20 14:49:20 +0900

    docs: 주석 추가

--- discord-clone-backend/routes/authRoutes.js
+++ discord-clone-backend/routes/authRoutes.js
@@ -2,12 +2,12 @@
 const express = require('express');
 const router = express.Router();
 
-// http://localhost:5002/api/auth/register
+// http://localhost:5002/api/auth/register : 회원가입
 router.post('/register', (request, response) => {
   response.send('register route');
 });
 
-// http://localhost:5002/api/auth/login
+// http://localhost:5002/api/auth/login : 로그인
 router.post('/login', (request, response) => {
   response.send('login route');
 });
--- discord-clone-backend/server.js
+++ discord-clone-backend/server.js
@@ -1,29 +1,29 @@
-// [1];
-const express = require('express');
-const http = require('http');
-const cors = require('cors');
-const mongoose = require('mongoose');
+// [1] 필요한 패키지 가져오기
+const express = require('express'); // express : nodejs 라이브러리
+const http = require('http'); // express 내장 http
+const cors = require('cors'); // cors 라이브러리 cors 에러 처리(?)
+const mongoose = require('mongoose'); // mongodb 연결 라이브러리
 require('dotenv').config();
 
-// [2] authRoutes.js 서버 라우트
+// [2] authRoutes.js 서버 라우트 : 파라미터? 생성
 const authRoutes = require('./routes/authRoutes');
 
-// [1]
+// [1] 사용자 로컬 URI 또는 하드코딩 5002포트
 const PORT = process.env.PORT || process.env.API_PORT;
 
-// [1]
+// [1] express로 json파싱, cors 라이브러리 사용
 const app = express();
 app.use(express.json());
 app.use(cors());
 
 // [2] register the Routes
-// http://localhost:5002/api/auth ~
+// 서버 BASE_URL + param을 http://localhost:5002/api/auth로 설정 | params는 authRoutes.js에 설정
 app.use('/api/auth', authRoutes);
 
-// [1]
+// [1] 서버 열기
 const server = http.createServer(app);
 
-// [1]
+// [1] mongodb 연결 | 성공, 에러 처리
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
