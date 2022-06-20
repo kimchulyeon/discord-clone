6aa20a2337740cdf1412b0b059e2c404eda6d83a
kim <kim@kimui-MacBookPro.local>
2022-06-20 17:54:05 +0900

    feat: mongoose Schema

--- discord-clone-backend/models/user.js
+++ discord-clone-backend/models/user.js
@@ -3,12 +3,12 @@ const mongoose = require('mongoose');
 // [5] 몽구스 스키마
 // 스키마를 기준으로 데이터를 DB에 넣기 전에 검사
 const userSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true, lowercase: true },
-  password: { type: String, required: true, trim: true },
-  nickname: { type: String },
-  // mail: { type: String, unique: true },
+  // mail: { type: String, required: true, unique: true, lowercase: true },
+  // password: { type: String, required: true, trim: true },
   // username: { type: String },
-  // password: { tpye: String },
+  mail: { type: String, unique: true },
+  username: { type: String },
+  password: { tpye: String },
 });
 
 module.exports = mongoose.model('user', userSchema);
