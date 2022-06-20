427d9b39c5b25a8a3a76732584cfce32dc1fa048
kim <kim@kimui-MacBookPro.local>
2022-06-20 17:52:54 +0900

    feat: POST login logic

--- discord-clone-backend/controllers/auth/postLogin.js
+++ discord-clone-backend/controllers/auth/postLogin.js
@@ -1,7 +1,30 @@
+const User = require('../../models/user');
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+
 const postLogin = async (request, response) => {
-  response.send({
-    username: 'wo',
-  });
+  try {
+    const { mail, password } = request.body;
+
+    const user = await User.findOne({ mail: mail.toLowerCase() });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // send new TOKEN
+      const token = 'JWT_TOKEN';
+
+      return response.status(200).json({
+        userDetails: {
+          mail: user.mail,
+          token,
+          username: user.username,
+        },
+      });
+    }
+
+    return response.status(400).send('Invalid credentials. Please try again');
+  } catch (err) {
+    return response.status(500).send('Something went wrong. Please try again');
+  }
 };
 
 module.exports = postLogin;
