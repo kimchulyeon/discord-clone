82391e2865d5fee703caed94c07e7bc179b042d2
kim <kim@kimui-MacBookPro.local>
2022-06-20 15:12:43 +0900

    feat: login & register auth controller 모듈화

--- discord-clone-backend/routes/authRoutes.js
+++ discord-clone-backend/routes/authRoutes.js
@@ -1,15 +1,19 @@
 // [2]
 const express = require('express');
 const router = express.Router();
+// [3] 모듈화
+const authControllers = require('../controllers/auth/authControllers');
 
 // http://localhost:5002/api/auth/register : 회원가입
-router.post('/register', (request, response) => {
+/* 
+'/register', (request, response) => {
   response.send('register route');
-});
+}
+이 형태였는데 컨트롤러폴더로 모듈을 분리 : exports.controllers = {postLogin, postRegister}
+*/
+router.post('/register', authControllers.controllers.postRegister);
 
 // http://localhost:5002/api/auth/login : 로그인
-router.post('/login', (request, response) => {
-  response.send('login route');
-});
+router.post('/login', authControllers.controllers.postLogin);
 
 module.exports = router;
