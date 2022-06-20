9b3685e4e5ad065ad53a69f248ec3be69c133fe3
kim <kim@kimui-MacBookPro.local>
2022-06-20 15:13:29 +0900

    feat: postLogin, postRegister 모듈화 파일 authRoutes.js파일로 전달

---
+++ discord-clone-backend/controllers/auth/authControllers.js
@@ -0,0 +1,8 @@
+const postLogin = require('./postLogin');
+const postRegister = require('./postRegister');
+
+// module.exports와 exports의 차이
+exports.controllers = {
+  postLogin,
+  postRegister,
+};
