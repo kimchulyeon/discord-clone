8f580cd9e7737429efbd05748903eb0fd2dd5d5b
kim <kim@kimui-MacBookPro.local>
2022-06-20 14:39:06 +0900

    feat: express server routes initiate

--- discord-clone-backend/server.js
+++ discord-clone-backend/server.js
@@ -1,17 +1,29 @@
+// [1];
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+// [2] authRoutes.js 서버 라우트
+const authRoutes = require('./routes/authRoutes');
+
+// [1]
 const PORT = process.env.PORT || process.env.API_PORT;
 
+// [1]
 const app = express();
 app.use(express.json());
 app.use(cors());
 
+// [2] register the Routes
+// http://localhost:5002/api/auth ~
+app.use('/api/auth', authRoutes);
+
+// [1]
 const server = http.createServer(app);
 
+// [1]
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
