d331e45da2562872e3a0f374bc1d9c22ef842629
KimChulyeon <guinness987@gmail.com>
2022-06-22 17:05:40 +0900

    feat: Routes settings

--- discord-clone-frontend/src/App.jsx
+++ discord-clone-frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, Navigate } from 'react-router-dom';
 import LoginPage from './pages/login/LoginPage';
 import RegisterPage from './pages/register/RegisterPage';
 import Dashboard from './pages/dashboard/Dashboard';
@@ -8,6 +8,7 @@ export default function App() {
   return (
     <>
       <Routes>
+        <Route path="/" element={<Navigate replace to="/dashboard" />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<Dashboard />} />
