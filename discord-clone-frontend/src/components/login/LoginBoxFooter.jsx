fda78c06006fdb80040a7b59a4bfb1946051a27b
kimchulyeon <guinness987@gmail.com>
2022-06-23 00:03:39 +0900

    feat: add disabled button tooltip

--- discord-clone-frontend/src/components/login/LoginBoxFooter.jsx
+++ discord-clone-frontend/src/components/login/LoginBoxFooter.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import CustomPrimaryBtn from "../../layouts/CustomPrimaryBtn";
 import RedirectInfo from "../../layouts/RedirectInfo";
 import { useNavigate } from "react-router-dom";
+import { Tooltip } from "@mui/material";
 
 export default function LoginFooter({ handleLogin, isFormValid }) {
   const navigate = useNavigate();
@@ -11,17 +12,26 @@ export default function LoginFooter({ handleLogin, isFormValid }) {
     navigate("/register");
   };
 
+  const getFormNotValidMessage = () => {
+    return "Enter correct e-mail address and password should contains between 6 and 12 characters";
+  };
+  const getFormValidMessage = () => {
+    return "Press to Log in!";
+  };
+
   return (
     <>
-      <div>
-        {/* 버튼 */}
-        <CustomPrimaryBtn
-          label="Log in"
-          additionalStyles={{ marginTop: "30px" }}
-          disabled={!isFormValid}
-          onClick={handleLogin}
-        />
-      </div>
+      <Tooltip title={!isFormValid ? getFormNotValidMessage() : getFormValidMessage()}>
+        <div>
+          {/* 버튼 */}
+          <CustomPrimaryBtn
+            label="Log in"
+            additionalStyles={{ marginTop: "30px" }}
+            disabled={!isFormValid}
+            onClick={handleLogin}
+          />
+        </div>
+      </Tooltip>
       {/* 회원가입 버튼 */}
       <RedirectInfo
         text="Need an account?"
