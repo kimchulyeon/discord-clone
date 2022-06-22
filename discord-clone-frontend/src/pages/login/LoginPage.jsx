3e38b30b5b0ca8a4928e22c8327c47b77db2d9bf
kimchulyeon <guinness987@gmail.com>
2022-06-23 00:18:16 +0900

    feat: add login validation check

--- discord-clone-frontend/src/pages/login/LoginPage.jsx
+++ discord-clone-frontend/src/pages/login/LoginPage.jsx
@@ -1,16 +1,22 @@
-import React, { useState } from 'react';
-import AuthBox from '../../layouts/AuthBox';
-import LoginBoxHeader from '../../components/login/LoginBoxHeader';
-import LoginInput from '../../components/login/LoginInput';
-import LoginBoxFooter from '../../components/login/LoginBoxFooter';
+import React, { useState, useEffect } from "react";
+import AuthBox from "../../layouts/AuthBox";
+import LoginBoxHeader from "../../components/login/LoginBoxHeader";
+import LoginInput from "../../components/login/LoginInput";
+import LoginBoxFooter from "../../components/login/LoginBoxFooter";
+import { validateLoginForm } from "../../assets/js/validators";
 
 export default function LoginPage() {
-  const [mail, setMail] = useState('');
-  const [password, setPassword] = useState('');
+  const [mail, setMail] = useState("");
+  const [password, setPassword] = useState("");
   const [isFormValid, setIsFormValid] = useState(false);
 
+  useEffect(() => {
+    setIsFormValid(validateLoginForm({ mail, password }));
+    console.log("check validation");
+  }, [mail, password, setIsFormValid]);
+
   const handleLogin = () => {
-    console.log('log in');
+    console.log(mail, password);
   };
 
   return (
