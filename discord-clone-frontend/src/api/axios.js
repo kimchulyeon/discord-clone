5e31805dd0060b04b681ffd34d127bc003bfdeb9
KimChulyeon <guinness987@gmail.com>
2022-06-24 15:53:37 +0900

    feat: 엑시오스 인터셉터 간략하게 작성

--- discord-clone-frontend/src/api/axios.js
+++ discord-clone-frontend/src/api/axios.js
@@ -1,26 +1,51 @@
 import axios from 'axios';
+import { logout } from '../assets/js/validators';
 
+// instance==============================================================================================================
 const instance = axios.create({
   baseURL: 'http://localhost:5002/api/auth/',
   timeout: 1000,
 });
 
-// 로그인
+// axios interceptor=====================================================================================================
+instance.interceptors.request.use(
+  (config) => {
+    const userDetail = localStorage.getItem('user');
+
+    if (userDetail) {
+      const token = JSON.parse(userDetail).token;
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (err) => {
+    return Promise.reject(err);
+  },
+);
+
+// login=================================================================================================================
 export const login = async (data) => {
   try {
     return await instance.post('login', data);
   } catch (err) {
-    console.log(err);
     return err;
   }
 };
 
-// 회원가입
+// register===============================================================================================================
 export const register = async (data) => {
   try {
     return await instance.post('register', data);
   } catch (err) {
-    console.log(err);
     return err;
   }
 };
+
+function checkResponseCode(err) {
+  const responseCode = err?.response?.status;
+
+  if (responseCode) {
+    (responseCode === 401 || responseCode === 403) && logout();
+  }
+}
